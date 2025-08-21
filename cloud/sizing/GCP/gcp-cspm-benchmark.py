12b361f3a6c4362487477f52348992f456bce502
cyttorak <103541642+gmatiasben@users.noreply.github.com>
2025-08-21 07:20:46 -0500

    Update gcp-cspm-benchmark.py

--- cloud/sizing/GCP/gcp-cspm-benchmark.py
+++ cloud/sizing/GCP/gcp-cspm-benchmark.py
@@ -99,7 +99,11 @@ def process_gcp_project(gcp_project: Project) -> Dict[str, Any]:
     if gcp_project.state == Project.State.DELETE_REQUESTED:
         log.info("Skipping GCP project %s (project pending deletion)", gcp_project.display_name)
         return {}
-
+    
+    # Linea nueva
+    if gcp_projects.parent == 
+        return {}
+    
     result = {
         "project_id": gcp_project.project_id,
         "kubenodes_running": 0,
