1c55fa6095a0ad2cee326ec9abe04769afa6cbd2
cyttorak <103541642+gmatiasben@users.noreply.github.com>
2025-10-28 12:23:16 -0300

    Update audit_event_count_per_vendor.cs

--- ngsiem/queries/audit_event_count_per_vendor.cs
+++ ngsiem/queries/audit_event_count_per_vendor.cs
@@ -1,5 +1,10 @@
+// Prerquisite: Configure 7d timeframe
+// Gives GB/day per vendor - v1.0.0
+// Matias Bendel by Crowdstrike
 eventSize() 
 | [sum("_eventSize", as=TotalBytes), groupby([#Vendor], function=([count(), sum("_eventSize", as=SizeBytes)]))]
+| TotalBytes:=TotalBytes/7
+| SizeBytes:=SizeBytes/7
 | SizeGB:=unit:convert("SizeBytes", binary=true, from=B, to=G, keepUnit=true)
 | TotalGB:=unit:convert("TotalBytes", binary=true, from=B, to=G, keepUnit=true)
 | sort(SizeBytes, order=desc, limit=10)
