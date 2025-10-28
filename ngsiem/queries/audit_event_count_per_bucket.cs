4cd485532c4cf0cf7e06717eb1eefa4c3a4ff2d4
cyttorak <103541642+gmatiasben@users.noreply.github.com>
2025-10-28 12:24:35 -0300

    Delete ngsiem/queries/audit_event_count_per_bucket.cs

--- ngsiem/queries/audit_event_count_per_bucket.cs
+++
@@ -1,6 +0,0 @@
-// Count number of events for the top 10 event buckets
-eventSize() 
-| [sum("_eventSize", as=total), groupby([#event_simpleName], function=([count(), sum("_eventSize", as=SizeBytes)]))]
-| SizeGB:=unit:convert("SizeBytes", binary=true, from=B, to=G, keepUnit=true)
-| sort(SizeBytes, order=desc, limit=10)
-| pct := (SizeBytes/total)*100 | format(format="%,.3f%%", field=[pct], as=pct)
