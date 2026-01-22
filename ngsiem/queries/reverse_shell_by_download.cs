e61d97a5696af2b1112c49166853c9bae227f741
cyttorak <103541642+gmatiasben@users.noreply.github.com>
2026-01-22 17:48:46 -0500

    Update reverse_shell_by_download.cs

--- ngsiem/queries/reverse_shell_by_download.cs
+++ ngsiem/queries/reverse_shell_by_download.cs
@@ -1,21 +1,16 @@
-// Reverse Shell Hunting - Suspicious file download - v1.0.0
+// Privilege Escalation Hunting CQL Query - v1.0.0
 // Matias Bendel by Crowdstrike
-#event_simpleName=/^(CommandHistory|ProcessRollup2)$/ event_platform=Win
+#event_simpleName=/^(AssociateIndicator|CommandHistory)$/
 | case{
-    // Check to see if event is CommandHistory
-    #event_simpleName=CommandHistory
-    // This is keyword list; modify as desired
-    | CommandHistory=/(curl|wget|Invoke-WebRequest)/i
-    // This puts the CommandHistory entries into an array
-    | CommandHistorySplit:=splitString(by="¶", field=CommandHistory)
-    // Search array for certain commands
-    | objectArray:eval(array="CommandHistorySplit[]", var=x, asArray="out[]", function={ x=/(curl|wget|Invoke-WebRequest)/i | out:=x }) | Command:=out[0];
-    // Check to see if event is ProcessRollup2. If yes, create mini process tree
-    #event_simpleName="ProcessRollup2" | ExecutionChain:=format(format="%s\n\t└ %s (%s)", field=[ParentBaseFileName, FileName, RawProcessId]);
+    // Check to see if event is AssociateIndicator
+    #event_simpleName=AssociateIndicator
+    // Filter by Tactic
+    | Tactic=/(Privilege Escalation)/i;
+    #event_simpleName=CommandHistory;
 }
-// Use selfJoinFilter to pair PR2 and CH events
-| selfJoinFilter(field=[aid, TargetProcessId], where=[{#event_simpleName="ProcessRollup2"}, {#event_simpleName="CommandHistory"}])
+// Use selfJoinFilter to pair AIn & CH events
+| selfJoinFilter(field=[aid, TargetProcessId], where=[{#event_simpleName="CommandHistory"}, {#event_simpleName="AssociateIndicator"}])
 // Aggregate to display details
-| groupBy([aid, TargetProcessId], function=([collect([ProcessStartTime, ComputerName, UserName, ExecutionChain, Command])]), limit=max)
-// Format ProcessStartTime to human-readable
-| ProcessStartTime:=ProcessStartTime*1000 | ProcessStartTime:=formatTime(format="%F %T.%L %Z", field="ProcessStartTime")
+| groupBy([aid, TargetProcessId], function=([collect([LogonTime, ComputerName, event_platform, UserName, Tactic, Technique, CommandHistory])]), limit=max)
+// Format LogonTime to human-readable
+| LogonTime:=LogonTime*1000 | LogonTime:=formatTime(format="%F %T.%L %Z", field="LogonTime")
