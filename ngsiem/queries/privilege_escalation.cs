f428e0d0eb0f5c8f1231e4c7f5d9c44035452da5
cyttorak <103541642+gmatiasben@users.noreply.github.com>
2026-01-22 17:49:09 -0500

    Update privilege_escalation.cs

--- ngsiem/queries/privilege_escalation.cs
+++ ngsiem/queries/privilege_escalation.cs
@@ -1,17 +1,16 @@
 // Privilege Escalation Hunting CQL Query - v1.0.0
 // Matias Bendel by Crowdstrike
-#event_simpleName=/^(AssociateIndicator|ProcessRollup2)$/
+#event_simpleName=/^(AssociateIndicator|CommandHistory)$/
 | case{
     // Check to see if event is AssociateIndicator
     #event_simpleName=AssociateIndicator
     // Filter by Tactic
     | Tactic=/(Privilege Escalation)/i;
-    // Check to see if event is ProcessRollup2. If yes, create mini process tree
-    #event_simpleName="ProcessRollup2" | ExecutionChain:=format(format="%s\n\t└ %s (%s)", field=[ParentBaseFileName, FileName, RawProcessId]);
+    #event_simpleName=CommandHistory;
 }
-// Use selfJoinFilter to pair PR2 and AIn events
-| selfJoinFilter(field=[aid, TargetProcessId], where=[{#event_simpleName="ProcessRollup2"}, {#event_simpleName="AssociateIndicator"}])
+// Use selfJoinFilter to pair AIn & CH events
+| selfJoinFilter(field=[aid, TargetProcessId], where=[{#event_simpleName="CommandHistory"}, {#event_simpleName="AssociateIndicator"}])
 // Aggregate to display details
-| groupBy([aid, TargetProcessId], function=([collect([ProcessStartTime, ComputerName, event_platform, UserName, Tactic, Technique, ExecutionChain, MD5HashData, CommandLine])]), limit=max)
-// Format ProcessStartTime to human-readable
-| ProcessStartTime:=ProcessStartTime*1000 | ProcessStartTime:=formatTime(format="%F %T.%L %Z", field="ProcessStartTime")
+| groupBy([aid, TargetProcessId], function=([collect([LogonTime, ComputerName, event_platform, UserName, Tactic, Technique, CommandHistory])]), limit=max)
+// Format LogonTime to human-readable
+| LogonTime:=LogonTime*1000 | LogonTime:=formatTime(format="%F %T.%L %Z", field="LogonTime")
